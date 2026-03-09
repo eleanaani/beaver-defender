840ac2ac65ba1309aa53186087a338d466d6e1b1
eleanaani <helenandreevnaa@gmail.com>
2026-03-09 13:41:23 +0300

    Start beaver block

--- index.cs
+++ index.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    private float vehicleSpeed = 15.0f;
+    private float turnSpeed = 20.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    public string inputID;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
